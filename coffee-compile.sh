7ebeece00af560e1bfa01f7a0ab80d70b898f56b
Ben Nevile <ben@mainsocial.com>
2012-06-25 21:11:40 -0400

    housekeeping

--- coffee-compile.sh
+++
@@ -1,4 +0,0 @@
-coffee -o lib/ -c src/
-coffee -o lib/models/ -c src/models/
-
-coffee -o test/lib/ -c test/src/
--- compile-and-test.sh
+++
@@ -1,4 +0,0 @@
-./coffee-compile.sh
-    
-node test/lib/controllers-test.js
-node test/lib/couch-views-test.js
